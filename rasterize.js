959d81f8dcb8556cb33a18a3fcc9972a8c6e97c1
ragabala <bragavendran@yahoo.com>
2017-11-17 19:09:24 -0500

    part 4 comeplete

--- rasterize.js
+++ rasterize.js
@@ -280,8 +280,59 @@ function handleKeyDown(event) {
           
     } // end switch
     if(flagEventTriggered)
+    {
+      sortModelsByDepth();
       renderModels();
+    }
 } // end handleKeyDown
+
+function setDistanceFromEye(model)
+{
+    var temp = vec3.create();
+    var tempCenter = vec3.create();
+
+  var distVect = vec3.subtract(temp,Eye,vec3.add(tempCenter,model.center,model.translation));
+  model.distance = vec3.dot(distVect,distVect)    
+
+}
+
+
+function sortModelsByDepth(){
+
+//console.log("sorting model by depth")
+models.forEach(function(model){setDistanceFromEye(model)
+//console.log(model.name+" "+model.distance)
+
+});
+models.sort(function(model1,model2)
+ {
+
+    //we need to ensure that models with alpha 1 needs to stay in the front of models
+    if(model1.alpha == 1)
+        return -1;
+    // sort in descending order of Depth
+    return model2.distance - model1.distance;
+
+  });
+
+//console.log("after sorting")
+
+writeDepthValues();
+
+//console.log(JSON.stringify(models,null,2))
+
+
+}
+
+function writeDepthValues(){
+
+models.forEach(function(model,index){
+    //console.log(model.name+" "+model.distance)
+var textbox = document.getElementById("box"+(index+1))
+textbox.value = model.name
+})
+
+}
 
 // set up the webGL environment
 function setupWebGL() {
@@ -455,6 +506,7 @@ function loadModels() {
                     model.alpha = model.material.alpha
                     model.type = modelEnum.TRIANGLES
                     model.index = whichSet
+                    model.name = model.type+" "+model.index
 
                     // set up hilighting, modeling translation and rotation
                     model.center = vec3.fromValues(0,0,0);  // center point of tri set
@@ -531,6 +583,7 @@ function loadModels() {
                     var model = inputEllipsoids[whichEllipsoid];
                     model.type = modelEnum.ELLIPSOID
                     model.index = whichEllipsoid
+                    model.name = model.type+" "+model.index
 
                     model.on = false; // ellipsoids begin without highlight
                     model.translation = vec3.fromValues(0,0,0); // ellipsoids begin without translation
@@ -827,7 +880,7 @@ function setDepthtoDefaults(){
 // render the loaded model
 function renderModels() {
    
-   setDepthtoDefaults();
+   //setDepthtoDefaults();
     
     // construct the model transform matrix, based on model state
     function makeModelTransform(currModel) {
@@ -899,6 +952,9 @@ function renderModels() {
     // this will generate the models in descending order by alpha value
     models.forEach(function(model,modelIndex){
           gl.uniform1f(aplhaULoc,model.alpha); // pass in the alpha component
+
+    if(model.alpha == 1)           
+        gl.depthMask(true);
 
     if(model.alpha < 1)  
     {  
@@ -984,136 +1040,11 @@ function renderModels() {
 
 
 
-
-// render the loaded model
-function renderModels1() {
-    
-    // construct the model transform matrix, based on model state
-    function makeModelTransform(currModel) {
-        var zAxis = vec3.create(), sumRotation = mat4.create(), temp = mat4.create(), negCtr = vec3.create();
-
-        // move the model to the origin
-        mat4.fromTranslation(mMatrix,vec3.negate(negCtr,currModel.center)); 
-        
-        // scale for highlighting if needed
-        if (currModel.on)
-            mat4.multiply(mMatrix,mat4.fromScaling(temp,vec3.fromValues(1.2,1.2,1.2)),mMatrix); // S(1.2) * T(-ctr)
-        
-        // rotate the model to current interactive orientation
-        vec3.normalize(zAxis,vec3.cross(zAxis,currModel.xAxis,currModel.yAxis)); // get the new model z axis
-        mat4.set(sumRotation, // get the composite rotation
-            currModel.xAxis[0], currModel.yAxis[0], zAxis[0], 0,
-            currModel.xAxis[1], currModel.yAxis[1], zAxis[1], 0,
-            currModel.xAxis[2], currModel.yAxis[2], zAxis[2], 0,
-            0, 0,  0, 1);
-        mat4.multiply(mMatrix,sumRotation,mMatrix); // R(ax) * S(1.2) * T(-ctr)
-        
-        // translate back to model center
-        mat4.multiply(mMatrix,mat4.fromTranslation(temp,currModel.center),mMatrix); // T(ctr) * R(ax) * S(1.2) * T(-ctr)
-
-        // translate model to current interactive orientation
-        mat4.multiply(mMatrix,mat4.fromTranslation(temp,currModel.translation),mMatrix); // T(pos)*T(ctr)*R(ax)*S(1.2)*T(-ctr)
-        
-    } // end make model transform
-
-    function bindImageTexture(model){  
-        gl.bindTexture(gl.TEXTURE_2D, model.textureObject);
-        
-    } // bind textures for each model
-    
-    // var hMatrix = mat4.create(); // handedness matrix
-    var pMatrix = mat4.create(); // projection matrix
-    var vMatrix = mat4.create(); // view matrix
-    var mMatrix = mat4.create(); // model matrix
-    var pvMatrix = mat4.create(); // hand * proj * view matrices
-    var pvmMatrix = mat4.create(); // hand * proj * view * model matrices
-    
-    //console.log("Checking animation")
-    //requestAnimationFrame(renderModels);
-    // set up frame render callback
-    
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // clear frame/depth buffers
-    
-    // set up projection and view
-    // mat4.fromScaling(hMatrix,vec3.fromValues(-1,1,1)); // create handedness matrix
-    mat4.perspective(pMatrix,0.5*Math.PI,1,0.1,10); // create projection matrix
-    mat4.lookAt(vMatrix,Eye,Center,Up); // create view matrix
-    mat4.multiply(pvMatrix,pvMatrix,pMatrix); // projection
-    mat4.multiply(pvMatrix,pvMatrix,vMatrix); // projection * view
-
-    // render each triangle set
-    var currSet; // the tri set and its material properties
-    for (var whichTriSet=0; whichTriSet<numTriangleSets; whichTriSet++) {
-        currSet = inputTriangles[whichTriSet];
-        
-        
-        // make model transform, add to view project
-        makeModelTransform(currSet);
-        mat4.multiply(pvmMatrix,pvMatrix,mMatrix); // project * view * model
-        gl.uniformMatrix4fv(mMatrixULoc, false, mMatrix); // pass in the m matrix
-        gl.uniformMatrix4fv(pvmMatrixULoc, false, pvmMatrix); // pass in the hpvm matrix
-        
-        // reflectivity: feed to the fragment shader
-        gl.uniform3fv(ambientULoc,currSet.material.ambient); // pass in the ambient reflectivity
-        gl.uniform3fv(diffuseULoc,currSet.material.diffuse); // pass in the diffuse reflectivity
-        gl.uniform3fv(specularULoc,currSet.material.specular); // pass in the specular reflectivity
-        gl.uniform1f(shininessULoc,currSet.material.n); // pass in the specular exponent
-        
-        // vertex buffer: activate and feed into vertex shader & Textures
-        gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffers[whichTriSet]); // activate
-        gl.vertexAttribPointer(vPosAttribLoc,3,gl.FLOAT,false,0,0); // feed
-        gl.bindBuffer(gl.ARRAY_BUFFER,normalBuffers[whichTriSet]); // activate
-        gl.vertexAttribPointer(vNormAttribLoc,3,gl.FLOAT,false,0,0); // feed
-        gl.bindBuffer(gl.ARRAY_BUFFER,textureBuffers[whichTriSet]); // activate
-        gl.vertexAttribPointer(texPosArrtibLoc,2,gl.FLOAT,false,0,0); // feed
-        bindImageTexture(currSet); // setting the textures 
-        // triangle buffer: activate and render
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,triangleBuffers[whichTriSet]); // activate
-        gl.drawElements(gl.TRIANGLES,3*triSetSizes[whichTriSet],gl.UNSIGNED_SHORT,0); // render
-        
-    } // end for each triangle set
-    
-    // render each ellipsoid
-    var ellipsoid, instanceTransform = mat4.create(); // the current ellipsoid and material
-    
-    for (var whichEllipsoid=0; whichEllipsoid<numEllipsoids; whichEllipsoid++) {
-        ellipsoid = inputEllipsoids[whichEllipsoid];     
-        // define model transform, premult with pvmMatrix, feed to vertex shader
-        makeModelTransform(ellipsoid);
-        pvmMatrix = mat4.multiply(pvmMatrix,pvMatrix,mMatrix); // premultiply with pv matrix
-        gl.uniformMatrix4fv(mMatrixULoc, false, mMatrix); // pass in model matrix
-        gl.uniformMatrix4fv(pvmMatrixULoc, false, pvmMatrix); // pass in project view model matrix
-
-        // reflectivity: feed to the fragment shader
-        gl.uniform3fv(ambientULoc,ellipsoid.ambient); // pass in the ambient reflectivity
-        gl.uniform3fv(diffuseULoc,ellipsoid.diffuse); // pass in the diffuse reflectivity
-        gl.uniform3fv(specularULoc,ellipsoid.specular); // pass in the specular reflectivity
-        gl.uniform1f(shininessULoc,ellipsoid.n); // pass in the specular exponent
-
-        gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffers[numTriangleSets+whichEllipsoid]); // activate vertex buffer
-        gl.vertexAttribPointer(vPosAttribLoc,3,gl.FLOAT,false,0,0); // feed vertex buffer to shader
-        gl.bindBuffer(gl.ARRAY_BUFFER,normalBuffers[numTriangleSets+whichEllipsoid]); // activate normal buffer
-        gl.vertexAttribPointer(vNormAttribLoc,3,gl.FLOAT,false,0,0); // feed normal buffer to shader
-        gl.bindBuffer(gl.ARRAY_BUFFER,textureBuffers[numTriangleSets+whichEllipsoid]); // activate tri buffer
-        gl.vertexAttribPointer(texPosArrtibLoc,2,gl.FLOAT,false,0,0); // feed normal buffer to shader
-        bindImageTexture(ellipsoid);// setting the textures         loadmo
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,triangleBuffers[numTriangleSets+whichEllipsoid]); // activate tri buffer
-        // draw a transformed instance of the ellipsoid
-        gl.drawElements(gl.TRIANGLES,triSetSizes[numTriangleSets+whichEllipsoid],gl.UNSIGNED_SHORT,0); // render
-    } // end for each ellipsoid
-
-} // end render model
-
-
-/* MAIN -- HERE is where execution begins after window load */
-
-
-
 function main() {
   
   setupWebGL(); // set up the webGL environment
   loadModels(); // load in the models from tri file
+  writeDepthValues();
   setupShaders(); // setup the webGL shaders
   setTextures();
   renderModels(); // draw the triangles using webGL
